cc838b8dab6ad185e8fd22f6b0adada438ee241f
никита <23018519+nikita@users.noreply.github.com>
2019-04-13 00:01:56 -0700

    Move App to its own component, create Popular component, add some new styles, and clean up some code.

--- app/index.js
+++ app/index.js
@@ -1,18 +1,9 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const App = require('./components/App');
 require('./index.css');
-
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                Hello World!
-            </div>
-        )
-    }
-}
 
 ReactDOM.render(
     <App />,
     document.getElementById('app')
-)
+);
