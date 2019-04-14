be68b1b538b8f80a7d9916eb5288e7e7c1b22b25
никита <23018519+nikita@users.noreply.github.com>
2019-04-14 13:03:34 -0700

    Create Results component / add it to our App routes, add PlayerPreview stateless function to Battle component, and some new css styles.

--- app/components/App.js
+++ app/components/App.js
@@ -6,6 +6,7 @@ const Switch = ReactRouter.Switch;
 const Nav = require('./Nav');
 const Home = require('./Home');
 const Battle = require('./Battle');
+const Results = require('./Results');
 const Popular = require('./Popular');
 
 class App extends React.Component {
@@ -17,6 +18,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/battle" component={Battle} />
+            <Route path="/battle/results" component={Results} />
             <Route path="/popular" component={Popular} />
             <Route render={function() {
               return <p>Not Found</p>
