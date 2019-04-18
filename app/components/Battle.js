1c8e79f566d78417dc620d43193a4450016be63c
никита <23018519+nikita@users.noreply.github.com>
2019-04-18 10:27:52 -0700

    Refactor Battle.js to ES6 syntax.

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -16,13 +16,9 @@ class PlayerInput extends React.Component {
     }
 
     handleChange(event) {
-        var value = event.target.value;
+        const value = event.target.value;
 
-        this.setState(function() {
-            return {
-                username: value
-            }
-        });
+        this.setState(() => ({ username: value }));
     }
 
     handleSubmit(event) {
@@ -35,23 +31,26 @@ class PlayerInput extends React.Component {
     }
     
     render() {
+        const { username } = this.state;
+        const { label } = this.props;
+
         return (
             <form className="column" onSubmit={this.handleSubmit}>
                 <label className="header" htmlFor="username">
-                    {this.props.label}
+                    {label}
                 </label>
                 <input
                     id="username"
                     placeholder="github username"
                     type="text"
                     autoComplete="off"
-                    value={this.state.username}
+                    value={username}
                     onChange={this.handleChange}
                 />
                 <button
                     className="button"
                     type="submit"
-                    disabled={!this.state.username}>
+                    disabled={!username}>
                     Submit
                 </button>
             </form>
@@ -81,29 +80,22 @@ class Battle extends React.Component {
     }
 
     handleSubmit(id, username) {
-        this.setState(function() {
-            var newState = {};
-            newState[id + "Name"] = username;
-            newState[id + "Image"] = "https://github.com/" + username + ".png?size=200";
-            return newState;
-        });
+        this.setState(() => ({
+            [id + "Name"]: username,
+            [id + "Image"]: `https://github.com/${username}.png?size=200`
+        }));
     }
 
     handleReset(id) {
-        this.setState(function() {
-            var newState = {};
-            newState[id + "Name"] = "";
-            newState[id + "Image"] = null;
-            return newState;
-        });
+        this.setState(() => ({
+            [id + "Name"]: "",
+            [id + "Image"]: null
+        }));
     }
 
     render() {
-        const match = this.props.match;
-        const playerOneName = this.state.playerOneName;
-        const playerTwoName = this.state.playerTwoName;
-        const playerOneImage = this.state.playerOneImage;
-        const playerTwoImage = this.state.playerTwoImage;
+        const { match } = this.props;
+        const { playerOneName, playerTwoName, playerOneImage, playerTwoImage } = this.state;
 
         return (
             <div>
@@ -121,7 +113,7 @@ class Battle extends React.Component {
                             username={playerOneName}>
                             <button
                                 className="reset"
-                                onClick={this.handleReset.bind(null, "playerOne")}>
+                                onClick={() => this.handleReset("playerOne")}>
                                 Reset
                             </button>
                         </PlayerPreview>}
@@ -139,7 +131,7 @@ class Battle extends React.Component {
                             username={playerTwoName}>
                             <button
                                 className="reset"
-                                onClick={this.handleReset.bind(null, "playerTwo")}>
+                                onClick={() => this.handleReset("playerTwo")}>
                                 Reset
                             </button>
                         </PlayerPreview>}
@@ -150,7 +142,7 @@ class Battle extends React.Component {
                         className="button"
                         to={{
                             pathname: match.url + "/results",
-                            search: `?playerOneName=` + playerOneName + "&playerTwoName=" + playerTwoName
+                            search: `?playerOneName=${playerOneName}&playerTwoName=${playerTwoName}`
                         }}>
                         Battle
                     </Link>}
